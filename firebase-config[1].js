c64ff25c92593a37b48c1cf30ba12e36543e7834
sabking008 <sabariking901@gmail.com>
2025-05-12 12:50:49 +0530

    Add files via upload

---
+++ firebase-config[1].js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwjRFS0fOiPy5UDgfra3CjyZPoBMFHEDM",
+  authDomain: "breake-pizza.firebaseapp.com",
+  databaseURL: "https://breake-pizza-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "breake-pizza",
+  storageBucket: "breake-pizza.firebasestorage.app",
+  messagingSenderId: "645127348538",
+  appId: "1:645127348538:web:402f21accdb8e3a87db003",
+  measurementId: "G-7JX0NMXPYJ"
+};
